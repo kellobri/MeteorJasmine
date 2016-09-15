2920cf982a41729d63aa07c50e182c50d8537779
kellobri <kellyobriant@gmail.com>
2016-09-15 15:04:56 -0400

    Creates specs for displaying uncompleted count and option to hide completed tasks

--- tests/jasmine/client/integration/todos/page-contents-spec.js
+++ tests/jasmine/client/integration/todos/page-contents-spec.js
@@ -4,8 +4,11 @@ describe ("the todo page : page contents", function() {
     expect($('title').text()).toEqual('Todo List');
   });
 
-  it ("should include a page heading of 'Todo List'", function() {
-    expect($('h1').text()).toEqual('Todo List');
+  it ("should include a page heading of 'Todo List' appended with the uncompleted task count", function(done) {
+    Meteor.setTimeout(function() {
+      expect($('h1').text()).toEqual('Todo List (0)');
+      done()
+    }, 400);
   });
 
   it ("should include an unordered list for displaying the tasks", function() {
@@ -17,6 +20,10 @@ describe ("the todo page : page contents", function() {
     var input = $('.new-task input');
     expect(input.length).toEqual(1);
     expect(input.attr('placeholder')).toEqual('Type to add new tasks');
+  });
+
+  it ("should include a checkbox for hiding completed tasks", function() {
+    expect($('label.hide-completed').find('input:checkbox').length).toEqual(1);
   });
 
 });
