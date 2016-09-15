4ee5daf60d1300e844e5f788f8ad8c529d2fc655
kellobri <kellyobriant@gmail.com>
2016-09-15 15:25:01 -0400

    Alters test data and creates spec for displaying incomplete tasks

--- tests/jasmine/client/integration/todos/task-list-spec.js
+++ tests/jasmine/client/integration/todos/task-list-spec.js
@@ -2,7 +2,7 @@ describe ("the todo page : task list", function() {
 
   beforeEach(function () {
     Meteor.call('fixtures.createTask', {
-      text: 'This is task 1', createdAt: '2015-01-01'
+      text: 'This is task 1', createdAt: '2015-01-01', completed: true
     });
     Meteor.call('fixtures.createTask', {
       text: 'This is task 2', createdAt: '2015-02-01'
@@ -15,16 +15,42 @@ describe ("the todo page : task list", function() {
     Meteor.call('fixtures.destroyTasks');
   });
 
-  it ("should contain the current list of tasks sorted by creation date descending", function(done) {
-    Meteor.setTimeout(function() {
-      var tasks = TodosSpecHelper.retrieveTasksFromUI();
+  describe ("show all tasks", function() {
 
-      expect(tasks.length).toEqual(3);
-      expect(tasks[0]).toEqual('This is task 3');
-      expect(tasks[1]).toEqual('This is task 2');
-      expect(tasks[2]).toEqual('This is task 1');
-      done();
-    }, 400);
-  });
+    it ("should contain the current list of tasks sorted by creation date descending", function(done) {
+      Meteor.setTimeout(function() {
+        var tasks = TodosSpecHelper.retrieveTasksFromUI();
+
+        expect(tasks.length).toEqual(3);
+        expect(tasks[0]).toEqual('This is task 3');
+        expect(tasks[1]).toEqual('This is task 2');
+        expect(tasks[2]).toEqual('This is task 1');
+        done();
+      }, 400);
+    });
+  })
+
+  describe ("show only the incomplete tasks", function() {
+
+    beforeEach(function() {
+      $('label.hide-completed').find('input:checkbox').click();
+    });
+
+    afterEach(function() {
+      $('label.hide-completed').find('input:checkbox').click();
+    });
+
+    it ("should contain the current list of incompleted tasks sorted by creation date descending", function(done) {
+      Meteor.setTimeout(function() {
+        var tasks = TodosSpecHelper.retrieveTasksFromUI();
+
+        expect(tasks.length).toEqual(2);
+        expect(tasks[0]).toEqual('This is task 3');
+        expect(tasks[1]).toEqual('This is task 2');
+        done();
+      }, 400);
+    })
+
+  })
 
 });
