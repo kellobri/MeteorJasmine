09935fef9895f21ed32f195d6ed42fba36a3aa2f
kellobri <kellyobriant@gmail.com>
2016-09-15 14:43:35 -0400

    Creates specs for reactivating a task

--- tests/jasmine/client/integration/todos/update-task-spec.js
+++ tests/jasmine/client/integration/todos/update-task-spec.js
@@ -11,32 +11,57 @@ describe ("the todo page : update task", function() {
     });
 
     it ("should set the 'completed' field to true", function(done) {
-      Meteor.setTimeout(function() {
-        // activate the checkbox
-        $('li').find("input:checkbox").click();
-
-        // find the associated record in the DB and verify it is checked
-        var tasks = Tasks.find().fetch();
-        expect(tasks.length).toEqual(1);
-        expect(tasks[0].completed).toEqual(true);
-
-        // ensure the checkbox is now checked
-        expect($('li').find("input:checkbox").is(':checked')).toEqual(true);
-        done();
-      }, 400);
+      toggleAndCheckTaskStatus(true, done);
     });
 
     it ("should show a strike-through for the completed tasks", function(done) {
+      toggleStatusAndCheckStrikeThru(true, done);
+    });
+  });
+
+  describe ("re-activating a task", function() {
+
+    beforeEach(function() {
+      Meteor.call('fixtures.createTask', {completed: true});
+    });
+
+    afterEach(function() {
+      Meteor.call('fixtures.destroyTasks');
+    });
+
+    it ("should set the 'completed' field to false", function(done) {
+      toggleAndCheckTaskStatus(false, done);
+    });
+
+    it ("should remove the strike-through for the re-activated tasks", function(done) {
+      toggleStatusAndCheckStrikeThru(false, done);
+    });
+  });
+
+  var toggleAndCheckTaskStatus = function(completeTask, done) {
       Meteor.setTimeout(function() {
-        $('li').find("input:checkbox").click();
+        // activate the checkbox
+        $("li").find("input:checkbox").click();
+
+        // find the associated record in the DB and verify
+        var tasks = Tasks.find().fetch();
+        expect(tasks[0].completed).toEqual(completeTask);
+
+        // verify the status of the UI checkbox
+        expect($("li").find("input:checkbox").is(':checked')).toEqual(completeTask);
+        done();
+      }, 400);
+    }
+
+    var toggleStatusAndCheckStrikeThru = function(completeTask, done) {
+      Meteor.setTimeout(function() {
+        $("li").find("input:checkbox").click();
       }, 400);
 
       Meteor.setTimeout(function() {
-        expect($('li').hasClass('checked')).toBe(true);
+        expect($("li").hasClass('checked')).toBe(completeTask);
         done();
       }, 800);
-    });
-    
-  });
+    }
 
 })
